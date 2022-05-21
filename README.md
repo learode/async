520826253c5204d72db3348cfd18e93a6c1b6045
mulfranck <mulfranck5@gmail.com>
2022-05-21 23:59:14 +0100

    All promise handlers are not executed:

---
+++ README.md
@@ -0,0 +1,6 @@
+No resolve or reject in the promise constructor the .then is not ran.
+Same as the `.catch`,  `.finally`.
+
+why?
+These methods are invoked by either a `resolve` or a `reject`.
+
--- asyn_code.js
+++ asyn_code.js
@@ -8,3 +8,11 @@ console.log(promise)
 promise.then(() => {
     console.log('what the hell is this')
 })
+
+promise.catch(() => {
+    console.log('Why is this')
+})
+
+promise.finally(() => {
+    console.log('this nigga should work right?')
+})
